5eb3173b14083800e8ed87c28c0b8d855e34f1cf
Gabester0 <gceipper@gmail.com>
2019-07-17 22:22:07 -0400

    Updates

---
+++ precache-manifest.3a0aecbc4af12bdb2dedc16d17bf9a65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ffc17fa485185754c0867c3481c417",
+    "url": "/Gabester0/good-brew/index.html/index.html"
+  },
+  {
+    "revision": "f41ba192e7a773d6c81d",
+    "url": "/Gabester0/good-brew/index.html/static/css/main.97ce96e7.chunk.css"
+  },
+  {
+    "revision": "ad90696bac55a857a99c",
+    "url": "/Gabester0/good-brew/index.html/static/js/2.8d6cc170.chunk.js"
+  },
+  {
+    "revision": "f41ba192e7a773d6c81d",
+    "url": "/Gabester0/good-brew/index.html/static/js/main.1c34c5a8.chunk.js"
+  },
+  {
+    "revision": "12eca2c21b65f2d4dd78",
+    "url": "/Gabester0/good-brew/index.html/static/js/runtime~main.81510221.js"
+  }
+]);
